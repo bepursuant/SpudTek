3dd45a84794e91c020be12933ea49fa4584c38f9
Kevin Klika <kklika@bepursuant.com>
2026-08-10 18:40:26 -0500

    Add automatic build script.

--- build.bat
+++ build.bat
@@ -1,1 +1,4 @@
-.\GIT_VERSION.scad.bat
+CALL .\GIT_VERSION.scad.bat
+
+SET OPENSCAD_PATH="C:\Program Files\OpenSCAD (Nightly)\openscad.exe"
+%OPENSCAD_PATH% -o "stl\ThreadedBarrelLiner_%GIT_BUILD%.stl" ThreadedBarrelLiner.scad
